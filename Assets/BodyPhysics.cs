bc5e2841b17e9cc4525c753d7067828d86a9a5f5
GiangVan2240 <giangvo532@gmail.com>
2025-06-15 13:25:01 +0700

    Giang

--- Assets/BodyPhysics.cs
+++ Assets/BodyPhysics.cs
@@ -7,25 +7,38 @@ public class BodyPhysics : MonoBehaviour
     [SerializeField] private bool useGravity = true;
     [SerializeField] private Vector3 gravity = new Vector3(0, -9.81f, 0);
 
-    private Vector3 velocity =Vector3.zero;
-    private Vector3 force =Vector3.zero;
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 force = Vector3.zero;
+
+
+    public void AddForce(Vector3 inForce)
+    {
+        force += inForce;
+    }
+
     private void Update()
     {
         if (useGravity)
         {
             ApplyGravity();
         }
-        ApplyForce();
 
+        // Apply forces to the body
+        ApplyForces();
+
+        // Update position based on velocity
         transform.position += velocity * Time.deltaTime;
     }
+
+    private void ApplyForces()
+    {
+        velocity += force * (Time.deltaTime / mass);
+        force = Vector3.zero;
+    }
+
     private void ApplyGravity()
     {
         velocity += gravity * (Time.deltaTime / mass);
-    }
-    private void ApplyForce()
-    {
-        velocity += force * (Time.deltaTime / mass);
     }
 
 }
--- Assets/DemoForceOnObject.cs
+++ Assets/DemoForceOnObject.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class DemoForceOnObject : MonoBehaviour
 {
-    [SerializeField] private Rigidbody targetRigidbody;
-    private void OnGUI()
-    {
-        //if (GUILayout.Button();
-    }
+    //[SerializeField] private Rigidbody targetRigidbody;
+    //private void OnGUI()
+    //{
+    //    //if (GUILayout.Button());
+    //}
 }
